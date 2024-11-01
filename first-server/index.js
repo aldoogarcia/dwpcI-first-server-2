2eb4abd2565a1757fc15d0aef413cda0a5f744ab
aldoogarcia <67659817+aldoogarcia@users.noreply.github.com>
2024-11-01 17:31:22 +0000

    Procesando Formulario

--- first-server/index.js
+++ first-server/index.js
@@ -1,6 +1,10 @@
 import http from "http";
+import path from "path";
+import { promises as fs } from 'fs';
 
-const server = http.createServer((req, res) => {
+global["__dirname"] = path.dirname(new URL(import.meta.url).pathname);
+
+const server = http.createServer(async (req, res) => {
   // Desestructurando de "req"
   let { url, method } = req;
 
@@ -8,7 +12,7 @@ const server = http.createServer((req, res) => {
 
   // Enrutando peticiones
   switch (url) {
-    case '/author':
+    case '/':
       // Peticion raiz
       // Estableciendo cabeceras
       res.setHeader('Content-Type', 'text/html');
@@ -16,12 +20,56 @@ const server = http.createServer((req, res) => {
       res.write(`
       <html>
         <head>
-          <link rel="icon" type="image/png" sizes="32x32" href="https://img.icons8.com/fluency/256/domain.png">
+          <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
           <title>My App</title>
+          <style>
+            body {
+              background-color: #ECF0F1;
+              font-family: Arial, sans-serif;
+            }
+            h1, h2 {
+              color: #3498DB;
+              text-align: center;
+              margin-top: 50px;
+            }
+            form {
+              margin-top: 30px;
+              text-align: center;
+            }
+            input[type="text"] {
+              width: 300px;
+              padding: 10px;
+              border: none;
+              border-radius: 5px;
+              box-shadow: 0px 0px 5px #3498DB;
+              outline: none;
+            }
+            button[type="submit"] {
+              background-color: #3498DB;
+              color: #fff;
+              border: none;
+              border-radius: 5px;
+              padding: 10px 20px;
+              cursor: pointer;
+              box-shadow: 0px 0px 5px #3498DB;
+              outline: none;
+            }
+            button[type="submit"]:hover {
+              background-color: #2980B9;
+            }
+          </style>
         </head>
         <body> 
           <h1 style="color: #333">Hello from my server</h1>
           <p style="color: #34495E">Estas en el recurso raiz.</p>
+          <h1>Hello from my server</h1>
+          <h2>Ingresa un mensaje</h2>
+          <div>
+            <form action="/message" method="POST">
+              <input type="text" name="message">
+              <button type="submit">Send</button>
+            </form>
+          </div>
         </body>
       </html>
       `);
@@ -31,33 +79,136 @@ const server = http.createServer((req, res) => {
       // Cerrando la comunicacion
       res.end();
       break;
-    case '/':
+    case '/author':
       // Peticion raiz
       // Estableciendo cabeceras
       res.setHeader('Content-Type', 'text/html');
-      let url_image = 'https://imgs.search.brave.com/DyrRUzETp1CaVbtx8TUbdWKMk7efLX_wikMthRJs8eU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90b3MtcHJlbWl1/bS9tdWplci1wZWNh/cy1jYXJhLW11ZXN0/cmEtcGVjYXMtcm9z/dHJvXzEyMjE5NTMt/MTk0MzMuanBnP3Nl/bXQ9YWlzX2h5YnJp/ZA';
+      let url_image = 'https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=';
       // Escribiendo la respuesta
       res.write(`
       <html>
         <head>
-          <link rel="icon" type="image/png" sizes="32x32" href="https://img.icons8.com/fluency/256/domain.png">
+          <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
           <title>My App</title>
         </head>
         <body style="text-align: center;">
           <h1 style="color: #333;">&#9889; Author &#9889;</h1>
-          <p style="color: #34495E;">Aldo Garcia Carrasco</p>
+          <p style="color: #34495E;">Ivan Rivalcoba Rivas - Web Developer</p>
           <div>
-            <img width="300px" src="https://imgs.search.brave.com/DyrRUzETp1CaVbtx8TUbdWKMk7efLX_wikMthRJs8eU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90b3MtcHJlbWl1/bS9tdWplci1wZWNh/cy1jYXJhLW11ZXN0/cmEtcGVjYXMtcm9z/dHJvXzEyMjE5NTMt/MTk0MzMuanBnP3Nl/bXQ9YWlzX2h5YnJp/ZA" alt="Foto Ivan Rivalcoba">
+            <img width="300px" src="https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=" alt="Foto Ivan Rivalcoba">
           </div>
         </body>
       </html>
       `);
-      console.log(`📣 Respondiendo: 200 ${url} ${method}`);
+      console.log(`📣 Respondiendo: 200 ${req.url} ${req.method}`);
       // Estableciendo codigo de respuesta
       res.statusCode = 200;
       // Cerrando la comunicacion
       res.end();
       break;
+    case "/favicon.ico":
+      // Especificar la ubicación del archivo de icono
+      const faviconPath = path.join(__dirname, 'favicon.ico');
+      try {
+        const data = await fs.readFile(faviconPath);
+        res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+        res.end(data);
+      } catch (err) {
+        console.error(err);
+        // Peticion raiz
+        // Estableciendo cabeceras
+        res.setHeader('Content-Type', 'text/html');
+        // Escribiendo la respuesta
+        res.write(`
+        <html>
+          <head>
+            <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
+            <title>My App</title>
+          </head>
+          <body> 
+            <h1>&#128534; 500 El server esta fuera de servicio</h1>
+            <p>Lo sentimos pero hubo un error en nuestro server...</p>
+            <p> ${err.message}</p>
+          </body>
+        </html>
+        `);
+        console.log(`📣 Respondiendo: 500 ${req.url} ${req.method}`);
+        // Estableciendo codigo de respuesta
+        res.statusCode = 500;
+        // Cerrando la comunicacion
+        res.end();
+      }
+      break;
+    case "/message":
+      // Verificando si es post
+      if (method === "POST") {
+        // Se crea una variable para almacenar los
+		    // Datos entrantes del cliente
+        let body = "";
+        // Se registra un manejador de eventos
+        // Para la recepción de datos
+        req.on("data", (data => {
+          body += data;
+          if (body.length > 1e6) return req.socket.destroy();
+        }));
+        // Se registra una manejador de eventos
+		    // para el termino de recepción de datos
+        req.on("end", () => {
+          // Procesa el formulario
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/html");
+          // Mediante URLSearchParams se extraen
+			    // los campos del formulario
+          const params = new URLSearchParams(body);
+          // Se construye un objeto a partir de los datos
+			    // en la variable params
+          const parsedParams = Object.fromEntries(params);
+          res.write(`
+          <html>
+            <head>
+              <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
+              <title>My App</title>
+              <style>
+                body {
+                  background-color: #f9f9f9;
+                  font-family: Arial, sans-serif;
+                }
+                h1 {
+                  color: #e74c3c;
+                  font-size: 48px;
+                  margin-top: 50px;
+                  text-align: center;
+                }
+                p {
+                  font-size: 24px;
+                  color: #7f8c8d;
+                  text-align: center;
+                  margin-top: 20px;
+                }
+                .error-message {
+                  font-size: 18px;
+                  color: #95a5a6;
+                  text-align: center;
+                  margin-top: 20px;
+                }
+              </style>
+            </head>
+            <body> 
+              <h1 style="color: #333">SERVER MESSAGE RECIEVED &#128172</h1>
+              <p>${parsedParams.message}</p>
+            </body>
+          </html>
+          `);
+          // Se finaliza la conexion
+          return res.end();
+        })
+      } else {
+        res.statusCode = 404;
+        res.write("404: Endpoint no encontrado")
+        res.end();
+      }
+      break;
+      // Continua con el defautl
     default:
       // Peticion raiz
       // Estableciendo cabeceras
@@ -66,7 +217,7 @@ const server = http.createServer((req, res) => {
       res.write(`
       <html>
         <head>
-          <link rel="icon" type="image/png" sizes="32x32" href="https://img.icons8.com/fluency/256/domain.png">
+          <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
           <title>My App</title>
         </head>
         <body> 
@@ -75,7 +226,7 @@ const server = http.createServer((req, res) => {
         </body>
       </html>
       `);
-      console.log(`📣 Respondiendo: 404 ${url} ${method}`);
+      console.log(`📣 Respondiendo: 404 ${req.url} ${req.method}`);
       // Estableciendo codigo de respuesta
       res.statusCode = 404;
       // Cerrando la comunicacion
